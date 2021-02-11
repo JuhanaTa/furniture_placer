f1b579fb93f0b1ced0d529ad1685f21cfae4a794
jalsson <nilsson.niklas.97@gmail.com>
2021-02-11 14:08:30 +0200

    Adding user data model and Room data model and 2 functions for creating and fetching rooms for user

---
+++ app/src/main/java/com/example/furniture_placer/data_models/Room.kt
@@ -0,0 +1,9 @@
+package com.example.furniture_placer.data_models
+
+import com.google.firebase.Timestamp
+
+data class Room (
+    val name: String? = null,
+    val created: Timestamp = com.google.firebase.Timestamp.now(),
+    val id: String? = null,
+    var itemCount: Int = 0)
--- app/src/main/java/com/example/furniture_placer/firebaseService.kt
+++ app/src/main/java/com/example/furniture_placer/firebaseService.kt
@@ -2,11 +2,14 @@ package com.example.furniture_placer
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import com.example.furniture_placer.data_models.Room
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.inject.Deferred
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 import java.sql.Timestamp
 
 class firebaseService {
@@ -38,6 +41,31 @@ class firebaseService {
             .addOnFailureListener { exception ->
                 Log.d(TAG, "user get failed with ", exception)
             }
+    }
+
+    suspend fun createRoom(name:String) : Room{
+        val user = firebaseAuth.currentUser
+            ?: return Room(name = name)
+        var uid = user.uid
+
+        val roomData = hashMapOf(
+            "name" to name,
+            "created" to Timestamp(System.currentTimeMillis()),
+            "itemCount" to 0
+        )
+        val doc = db.collection("users").document(uid).collection("rooms").add(roomData).await()
+        return Room(name = name,id = doc.id,itemCount = 0)
+    }
+
+    suspend fun getUserRooms() : List<Room?> {
+        val user = firebaseAuth.currentUser
+            ?: return mutableListOf<Room>()
+
+        var uid = user.uid
+
+        val query = db.collection("users").document(uid).collection("rooms").get().await()
+
+        return query.documents.map{doc ->  doc.toObject<Room>()}
     }
 
 }
